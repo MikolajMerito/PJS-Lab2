a53816c5fd97b98121dd5cc4bfbacc7f9887eaeb
Mikolaj <m.iksrigz@gmail.com>
2025-11-09 17:32:18 +0100

    koniec zajec

--- zad1/script.js
+++ zad1/script.js
@@ -1,2 +1,11 @@
-let message = "Hello World";
-console.log(message);
+let navigation = document.getElementById("navigation");
+let listElement = document.getElementsByClassName("list-element");
+let pageTitle = document.querySelector("#page-title");
+// let navLink = document.querySelectorAll(".nav-link[data-value]=\"1\"");
+let navLink = document.querySelectorAll(".nav-link");
+
+console.log(navigation);
+console.log(listElement);
+console.log(pageTitle);
+console.log(navLink);
+
